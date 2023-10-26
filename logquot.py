1835162837c12db96e9ff452906d51086fdb8522
Stefan Helmert <s.helmert@hzdr.de>
2023-10-26 08:50:43 +0200

    optimization, numba

--- logquot.py
+++ logquot.py
@@ -1,16 +1,20 @@
+from numba import jit
+
 
 from math import log, floor
 
 def q(x):
     return 3**x/2**floor(log(3)/log(2)*x+1)
 
+@jit
 def e(x):
-    return log(3)/log(2)*x - floor(log(3)/log(2)*x+1)
+    ex = log(3)/log(2)*x
+    return ex - floor(ex + 1)
 
 def q2(x):
     return 2**e(x)
     
-
+@jit
 def calc_maxima(n: int):
     m = -1
     r = []
@@ -21,4 +25,7 @@ def calc_maxima(n: int):
             r.append((i,2**m))
     return r
 
-print(calc_maxima(10))
+l = calc_maxima(10**11)
+
+for i, m in l:
+    print(f"{i:10} {m}")
